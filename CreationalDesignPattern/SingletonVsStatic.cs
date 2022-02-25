75fc59112d1465b354a3e5743d58b3ac92f5b645
gauravc574 <gauravchawre580@gmail.com>
2022-02-25 12:47:48 +0530

    Sigleton vs Static

---
+++ CreationalDesignPattern/SingletonVsStatic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalDesignPattern
+{
+    internal class SingletonVsStatic
+    {
+        //Differences between Singleton and static classes
+        //    1. Static is a keyword and Singleton is a design pattern
+        //    2. Static classes can contain only static members
+        //    3. Singleton is an object creational pattern with one instance of the class
+        //    4. Singleton can implement interfaces, inherit from other classes and it aligns with the OOPS concepts
+        //    5. Singleton object can be passed as a reference
+        //    6. Singleton supports object disposal
+        //    7. Singleton object is stored on heap
+        //    8. Singleton objects can be cloned
+    }
+}
